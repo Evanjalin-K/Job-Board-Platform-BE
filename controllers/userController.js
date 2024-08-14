19e89769f1d7b95408f79313008e180f9a385c1d
Evanjalin-K <jojaev510@gmail.com>
2024-08-14 15:56:34 +0530

    Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -252,7 +252,6 @@ updateProfessionalInfo: async (request, response) => {
         response.cookie('token', token, {
             httpOnly: true,
             secure: true,
-            SameSite:"none",
             expires: new Date(Date.now() + 24 * 3600000)
         });
         response.status(200).json({message: 'Login Successfull'})
